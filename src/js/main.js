a9dcb5640574264751ef8615410b55793c534a82
Morne Maritz <mornemaritz@gmail.com>
2025-08-25 16:15:56 +0200

    Replace live-server with vite. Add the first form to retriev a WebID

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import { Parser } from 'n3';
 
 document.addEventListener('DOMContentLoaded', () => {
     const actionLabels = document.querySelectorAll('.action-label');
@@ -28,5 +29,53 @@ document.addEventListener('DOMContentLoaded', function() {
         expandableContent.classList.toggle('expanded');
         bottomRow.classList.toggle('expanded');
         expandToggle.textContent = expandableContent.classList.contains('expanded') ? 'Collapse' : 'Expand';
+    });
+});
+
+document.getElementById("webid-form").addEventListener("submit", async e => {
+    e.preventDefault();
+    
+    const webIdInput = document.getElementById("webid-input");
+    const webId = webIdInput.value;
+
+    await fetch(webId)
+    .then(async response => {
+        if (!response.ok) {
+            if(response.status < 500)
+            {
+                const errorResponse = await response.json();
+                throw errorResponse;
+            }
+        } 
+        return await response.text()
+    })
+    .then(responseText => {
+        console.log(responseText);
+        var parser = new Parser();
+        parser.parse(responseText,
+            (err, quad, prefixes) => {
+                if (quad) {
+                    console.log(`quad.object.value: ${quad.object.value}`);
+                    console.log(`quad.predicate.value: ${quad.predicate.value}`);
+                    console.log(`quad.subject.value: ${quad.subject.value}`);
+                    console.log("-----------------------------------------");
+
+                    if (quad.predicate.id == "http://www.w3.org/ns/solid/terms#oidcIssuer") {
+                        localStorage.setItem("solid_oidc_issuer", quad.object.value)
+                        document.getElementById("solid_oidc_issuer").innerText = quad.object.value;
+                        document.getElementById("solid_oidc_issuer_div").classList = "";
+                    }
+                } else if (err) {
+                    console.error(`item not parseable as an n3 quad`, err);
+                } else {
+                    console.log("Prefixes", prefixes);
+                }
+            })
+    })
+    .catch(e => {
+        console.error(e);
+
+        document.getElementById("error_details").innerText = e.error+"\n\n"+e.error_description;
+        document.getElementById("error").classList = "";
     });
 });
